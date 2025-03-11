6e0b87f781304edaca7375f511a07f519cd914c0
Manish_Syal <manishsyal77@gmail.com>
2025-03-11 17:30:26 +0530

    gave custom config to AuthenticationManager, and created login rout to login the user and verify if its present or not in db.

--- src/main/java/com/manish/SpringSecEx/config/SecurityConfig.java
+++ src/main/java/com/manish/SpringSecEx/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.manish.SpringSecEx.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,7 +30,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(customizer ->customizer.disable());
-        http.authorizeHttpRequests(request-> request.anyRequest().authenticated()); // no one should be able to access the page without authentication for any request.
+        http.authorizeHttpRequests(request-> request
+                .requestMatchers("register","login") //these two routs can be accessed without authentication
+                .permitAll()
+                .anyRequest().authenticated()); // no one should be able to access any page without authentication for any request.
         //http.formLogin(Customizer.withDefaults()); // creating a default login form for the user
         http.httpBasic(Customizer.withDefaults()); // enabling for Postman as well (just for testing Rest API's)
         http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // making it stateless http // every time(on every request) user will get new session id
@@ -53,6 +58,10 @@ public class SecurityConfig {
 //        return new InMemoryUserDetailsManager(user1,user2);
 //    }
 
+    //step 1: user authobj goes to server.
+    //step 2: server has Authentication manager.
+    //step 3: Authentication manager call the AuthenticationProvider to do the work.
+    //so here we are giving custom configuring to the AuthenticationProvider.
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
@@ -60,5 +69,11 @@ public class SecurityConfig {
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+
+    //and here we are giving custom configuration to AuthenticationManager
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager(); // getting the object of AuthenticationManager interface from the implementing class(AuthenticationConfiguration).
     }
 }
--- src/main/java/com/manish/SpringSecEx/service/UserService.java
+++ src/main/java/com/manish/SpringSecEx/service/UserService.java
@@ -3,6 +3,9 @@ package com.manish.SpringSecEx.service;
 import com.manish.SpringSecEx.model.Users;
 import com.manish.SpringSecEx.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +14,23 @@ public class UserService {
     @Autowired
     private UserRepo repo;
 
+    @Autowired
+    AuthenticationManager authManager;
     public Users register(Users user){
         return repo.save(user);
 
+    }
+
+    public String verify(Users user) {
+        // so we are passing an un-authenticated object to authManager, and it will give us an authenticated object
+        // this will automatically verify the user is present in the db or not and return its obj if present in db.
+        Authentication authentication=
+                authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return "Success";
+        }
+
+        return "Fail";
     }
 }
